0fec1c1a9d0a04da7b432af7c42dd3ed1fcc057a
Gerardo Jandres <gjandres@grupopublimovil.local>
2021-04-22 11:15:40 -0600

    agregar name a formulario

--- resources/views/cellphones/create.blade.php
+++ resources/views/cellphones/create.blade.php
@@ -7,36 +7,36 @@
         @csrf
         <div class="col-md-6">
           <label for="model" class="form-label" >Modelo</label>
-          <input type="text" class="form-control" id="model" value="{{ old('model') }}">
+          <input type="text" class="form-control" name="model" id="model" value="{{ old('model') }}">
         </div>
         <div class="col-md-6">
           <label for="imei" class="form-label" >imei</label>
-          <input type="text" class="form-control" id="imei" value="{{ old('imei') }}">
+          <input type="text" class="form-control" name="imei" id="imei" value="{{ old('imei') }}">
         </div>
         <div class="col-12">
           <label for="brand" class="form-label" >Marca</label>
-          <input type="text" class="form-control" id="brand" value="{{ old('brand') }}" >
+          <input type="text" class="form-control" name="brand" id="brand" value="{{ old('brand') }}" >
         </div>
         <div class="col-12">
           <label for="number" class="form-label" >Numero</label>
-          <input type="text" class="form-control" id="number" value="{{ old('number') }}">
+          <input type="text" class="form-control" name="number" id="number" value="{{ old('number') }}">
         </div>
         <div class="col-md-4">
           <label for="company" class="form-label" >Compañia</label><br>
-          <select id="company" class="form-select" value="{{ old('company') }}">
+          <select name="company" id="company" class="form-select" value="{{ old('company') }}">
             <option selected>Choose...</option>
-            <option>Publimovil</option>
-            <option>Publimagen</option>
-            <option>Urbman</option>
+            <option value="1">Publimovil</option>
+            <option value="2">Publimagen</option>
+            <option value="3">Urbman</option>
           </select>
         </div>
         <div class="col-md-6">
           <label for="department" class="form-label" >Departamento</label>
-          <input type="text" class="form-control" id="department" value="{{ old('department') }}">
+          <input type="text" class="form-control" name="department" id="department" value="{{ old('department') }}">
         </div>
         <div class="col-md-2">
           <label for="employee" class="form-label" >Empleado</label>
-          <input type="text" class="form-control" id="employee" value="{{old('employee')}}">
+          <input type="text" class="form-control" name="employee" id="employee" value="{{old('employee')}}">
         </div>
         <br>
         <div class="col-6">
